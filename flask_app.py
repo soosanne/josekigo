8ba3009a19e82b3945cd4745be3d73962ff0511a
Soosanne <soosanne@mail.yahoo.com>
2023-02-23 17:37:21 +0000

    First try using a template

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/recordgameresult')
-def record_game_result():
-    return 'Game result recorded'
+@app.route("/")
+def index():
+    return render_template("main_page.html")
