1f7e7d793649d124353f0ff513f090250ecb860f
Soosanne <soosanne@mail.yahoo.com>
2023-02-23 21:56:39 +0000

    checkpoint - started adding database handling

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,27 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="Soosanne",
+    password="soos_mysql",
+    hostname="Soosanne.mysql.pythonanywhere-services.com",
+    databasename="Soosanne$gogames",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 comments = []
 
