34f890932e26c161a4fc2f218b3cb50d51d8e27f
Soosanne <soosanne@mail.yahoo.com>
2023-05-01 12:52:36 +0000

    Added tournaments and tournament text

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
+from flask_httpauth import HTTPBasicAuth
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import re
@@ -7,6 +8,7 @@ import database_connection
 app = Flask(__name__)
 app.config["DEBUG"] = True
 items_per_page = 20
+auth = HTTPBasicAuth()
 
 #############################
 # Database connection stuff #
@@ -39,16 +41,32 @@ class Game(db.Model):
     game_komi = db.Column(db.Float, default=0.5, nullable=False)
     game_winner = db.Column(db.String(255), db.ForeignKey('players.player_uuid'), nullable=False)       # Player UUID
     game_score = db.Column(db.String(10), nullable=False)                                               # String showing score or "resign"
+    game_tournament = db.Column(db.Integer, db.ForeignKey('tournaments.tournament_id'), nullable=True)
 
     game_whiteplayerdata = db.relationship('Player', foreign_keys='Game.game_whiteplayer')
     game_blackplayerdata = db.relationship('Player', foreign_keys='Game.game_blackplayer')
     game_winnerdata = db.relationship('Player', foreign_keys='Game.game_winner')
+    game_tournamentdata = db.relationship('Tournament', foreign_keys='Game.game_tournament')
 
     game_sgf = db.Column(db.Text, nullable=True)
+
+class Tournament(db.Model):
+    __tablename__ = "tournaments"
+    tournament_id = db.Column(db.Integer, primary_key=True)
+    tournament_name = db.Column(db.String(255), nullable=False)
+    tournament_text = db.Column(db.Text, nullable=True)
 
 #############
 # Functions #
 #############
+
+# Authentication function. Returns true if the username and password are correct
+@auth.verify_password
+def verify(username, password):
+    if username == 'admin' and password == 'powerhungry':
+        return True
+    else:
+        return False
 
 # Used on games lists pages to show how old a game is
 def time_ago(dt):
@@ -116,12 +134,19 @@ def index():
 def about():
     return render_template("about.html")
 
-#################
-# View Game SGF #
-#################
+#############
+# View Game #
+#############
 @app.route('/game/<int:gameid>', methods=["GET"])
 def view_game(gameid):
     return render_template('game_sgf.html', game=Game.query.get(gameid))
+
+#########################
+# View all players page #
+#########################
+@app.route('/players', methods=["GET"])
+def all_players():
+    return render_template('all_players.html', players=Player.query.order_by(Player.player_name).all())
 
 ####################
 # View player page #
@@ -133,12 +158,36 @@ def view_player(playeruuid):
     games=Game.query.filter((Game.game_whiteplayer==playeruuid) | (Game.game_blackplayer==playeruuid)).order_by(Game.game_datetime.desc()).paginate(page, per_page=items_per_page)
     return render_template('player.html', player=Player.query.get(playeruuid), games=games, time_ago=time_ago)
 
-#########################
-# View all players page #
-#########################
-@app.route('/players', methods=["GET"])
-def all_players():
-    return render_template('all_players.html', players=Player.query.all())
+#############################
+# View all tournaments page #
+#############################
+@app.route('/tournaments', methods=["GET"])
+def all_tournaments():
+    return render_template('tournaments.html', tournaments=Tournament.query.all())
+
+########################
+# View tournament page #
+########################
+@app.route('/tournament/<string:tournament_id>', methods=["GET"])
+def view_tournament(tournament_id):
+    global items_per_page
+    page = request.args.get('page', 1, type=int)
+    games=Game.query.filter(Game.game_tournament == tournament_id).order_by(Game.game_datetime.desc()).paginate(page, per_page=items_per_page)
+    return render_template('tournament.html', tournament=Tournament.query.get(tournament_id), games=games, time_ago=time_ago)
+
+########################
+# Edit tournament text #
+########################
+@app.route('/edit_tournament_text/<string:tournament_id>', methods=["GET", "POST"])
+@auth.login_required
+def edit_tournament_text(tournament_id):
+    if request.method == "GET":
+        return render_template('edit_tournament_text.html', tournament=Tournament.query.get(tournament_id))
+    else:
+        tournament = Tournament.query.get(tournament_id)
+        tournament.tournament_text = request.form["htmltext"]
+        db.session.commit()
+        return redirect(url_for('view_tournament', tournament_id=tournament_id))
 
 ###################
 # Upload SGF page #
@@ -150,6 +199,15 @@ def uploadsgf():
 
     # Get the sgfdata - this is what we will insert into the database
     sgfdata = request.data.decode("utf-8")
+
+    # Get the event ID - if there is one
+    # If the event is given AND is numeric AND exists in the DB
+    # then set the tournamentid to be used when we create the game record
+    event = findmatch("EV", sgfdata)
+    if event != "" and event.isnumeric() and db.session.query(Tournament.tournament_id).filter_by(tournament_id=int(event)).scalar() is not None:
+        tournamentid = int(event)
+    else:
+        tournamentid = None
 
     # Get all the important game information from the SGF
     whiteplayername = findmatch("PW", sgfdata)
@@ -191,14 +249,25 @@ def uploadsgf():
             db.session.commit()
 
     # Create game object
-    game = Game(game_whiteplayer = whiteplayeruuid,
-                game_blackplayer = blackplayeruuid,
-                game_boardsize = boardsize,
-                game_handicap = handicap,
-                game_komi = komi,
-                game_winner = winneruuid,
-                game_score = score,
-                game_sgf = sgfdata)
+    if tournamentid != None:
+        game = Game(game_whiteplayer = whiteplayeruuid,
+                    game_blackplayer = blackplayeruuid,
+                    game_boardsize = boardsize,
+                    game_handicap = handicap,
+                    game_komi = komi,
+                    game_winner = winneruuid,
+                    game_score = score,
+                    game_tournament = tournamentid,
+                    game_sgf = sgfdata)
+    else:
+        game = Game(game_whiteplayer = whiteplayeruuid,
+                    game_blackplayer = blackplayeruuid,
+                    game_boardsize = boardsize,
+                    game_handicap = handicap,
+                    game_komi = komi,
+                    game_winner = winneruuid,
+                    game_score = score,
+                    game_sgf = sgfdata)
 
     # Add game to DB
     db.session.add(game)
